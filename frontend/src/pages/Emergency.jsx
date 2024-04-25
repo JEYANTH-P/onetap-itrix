bb6e5d20dda5592708bb383f1a238b782c3d3e1e
Rishi-kumar18 <rk18102004@gmail.com>
2024-04-25 18:26:48 +0530

    emergency

--- frontend/src/pages/Emergency.jsx
+++ frontend/src/pages/Emergency.jsx
@@ -5,6 +5,8 @@ function Emergency() {
   return (
     <>  
         <Button onClick={() => window.open("https://rishi-kumar18.github.io/emergencyrepo/", "_blank")}>Ambulance</Button>
+        <Button onClick={() => window.open("https://rishi-kumar18.github.io/emergencyrepo/", "_blank")}>Fire</Button>
+        <Button onClick={() => window.open("https://rishi-kumar18.github.io/emergencyrepo/", "_blank")}>Police</Button>
     </>
   )
 }
