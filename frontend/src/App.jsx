1ffabad9c0beb5b307547c63ffcaa718e14b5b48
Rishi-kumar18 <rk18102004@gmail.com>
2024-04-25 16:25:44 +0530

    adasd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
     
             <Route path="/" element={<Home/>}/>
             <Route path="/emergency" element={<Emergency/>}/>
+            
          
         </Routes>
       </Router>
