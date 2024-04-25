5f9ff0dc6cb67aaea02e3d223f802bcfb8b3e284
JEYANTH-P <jeyabalan764@gmail.com>
2024-04-25 07:40:42 +0530

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import  Home from "./pages/Home"
+import { Route,Routes,BrowserRouter as Router } from 'react-router-dom'
+
+function App() {
+  
+
+  return (
+    <>
+      <Router>
+        <Routes>
+    
+            <Route path="/" element={<Home/>}/>
+         
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import  Map from '../components/map'
+
+function Home() {
+  return (
+    <>
+        <Map />
+        <div>Home</div>
+    </>
+
+  )
+}
+
+export default Home
