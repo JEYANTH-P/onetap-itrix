10a7ed3fe3aa27c48753beba79fae81962ce7971
JEYANTH-P <jeyabalan764@gmail.com>
2024-04-25 10:21:27 +0530

    few chaneg

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,13 @@
 import  Map from '../components/map'
 import WebcamCapture from '../components/cam'
-import  userLocation from '../components/userLocation'
+
 
 function Home() {
   return (
     <>  
         <Map />
-        <userLocation />
         <div>Home</div>
         <WebcamCapture />
-
     </>
 
   )
